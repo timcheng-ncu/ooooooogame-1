2670df41bc581543645e6e38579824a36764aec6
s8484842 <s8484842@gmail.com>
2017-03-16 00:07:12 +0800

    Control

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
 		Control.start();
+		Data.start();
+		Graphics.start();
+		Logic.start();
+		Network.start();
+		Sound.start();
 	}
 }
